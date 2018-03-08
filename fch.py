86414ac1c9cc006b568104d959e3b15bda365f15
yotakih <37173608+yotakih@users.noreply.github.com>
2018-03-08 19:07:41 +0900

    update

--- fch.py
+++ fch.py
@@ -2,30 +2,25 @@
 #-*-coding:utf-8-*-
 
 import os
+import sys
 
-findheaderdir = [
-	'/usr/include/'
-	,'/usr/include/x86_64-linux-gnu/'
-]
-
-hd = []
-
-def addhd(txt):
-	global hd 
-	hd += [txt]
-
-def findhd(txt):
-	for d in findheaderdir:
-		for h in hd:
-			p = os.path.join(d,h)
-			if os.path.exists(p):
-				with open(p, 'r') as f:
+def find(txt):
+	print('--------find result--------')
+	for r,d,fs in os.walk('/usr/include/'):
+		for f in fs:
+			if os.path.splitext(f)[1] == '.h':
+				p = os.path.join(r,f)
+				with open(p) as f:
 					for l in f.readlines():
 						if txt in l:
 							print('%s\t%s' % (p, l))
 
+def main():
+	if len(sys.argv) < 2:
+		print('error args. please select find works!!')
+		sys.exit()
+	find(sys.argv[1])
+
 if __name__ == '__main__':
-	#test
-	addhd('net/ethernet.h')
-	findhd('u_int8_t ether')
+	main()
 
